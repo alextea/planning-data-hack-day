16b98eccc9077f3d40c4fc26e342abcd7218aa76
Alex Torrance <alex.torrance@tpximpact.com>
2025-05-01 10:47:59 +0100

    Add application look up form

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,14 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/application', (req, res) => {
+  const applicationID = req.session.data['application_id']
+
+  res.render('application.html')
+})
+
+router.get('/application/:applicationID', (req, res) => {
+  const locals = {}
+
+  res.render('application.html', locals)
+})
